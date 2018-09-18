124a6af5158d7e30827fb3d8e2fabc35f95f1829
Jesus <neversaydie.jr@gmail.com>
2018-09-18 08:45:36 +0300

    Version 1.1.100

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,22 +1,23 @@
 $(function(){
-	var elements = $('.show');
-	console.log(elements);
-	for (var i = 0; i < elements.length; i++) {
-		$(elements[i]).addClass('start');
-		$(elements[i]).on("mouseenter", function(event){
-			$(event.target).toggleClass("start run");
+	if($(window).height() < $(window).width()){
+		var elements = $('.show');
+		for (var i = 0; i < elements.length; i++) {
+			$(elements[i]).addClass('start');
+			$(elements[i]).on("mouseenter", function(event){
+				$(event.target).toggleClass("start run");
+			});
+		}
+
+		$(document).scroll(function(){
+			for (var i = 0; i < elements.length; i++) {
+				if ($(document).scrollTop() + $(window).height() > $(elements[i]).offset().top &&
+					$(document).scrollTop() - $(elements[i]).offset().top < $(elements[i]).height()) {
+					$(elements[i]).mouseenter();
+					$(elements[i]).off('mouseenter');
+					elements.splice(i,i+1);
+					break;
+				}
+			}
 		});
 	}
-
-	$(document).scroll(function(){
-		for (var i = 0; i < elements.length; i++) {
-			if ($(document).scrollTop() + $(window).height() > $(elements[i]).offset().top &&
-				$(document).scrollTop() - $(elements[i]).offset().top < $(elements[i]).height()) {
-				$(elements[i]).mouseenter();
-				$(elements[i]).off('mouseenter');
-				elements.splice(i,i+1);
-				break;
-			}
-		}
-	});
 });
