882a9ce7c24f5baa116a14f0d2cc9fe36b174779
Jesus <neversaydie.jr@gmail.com>
2019-03-29 21:14:11 +0200

    pc11

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,4 +1,4 @@
-if(window.innerWidth > window.innerHeight && window.innerWidth >= 1100) {
+if(window.innerWidth > window.innerHeight && window.innerWidth > 1190) {
 	window.location.href = "pc.html";
 } else {
 	console.log(window.innerWidth);
