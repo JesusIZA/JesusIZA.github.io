7767cc074fa2e18eb782f8ec02a44099c5fe6b8b
Jesus <neversaydie.jr@gmail.com>
2019-03-29 21:03:35 +0200

    mob1

--- scripts/simpleAnimate.js
+++ scripts/simpleAnimate.js
@@ -11,12 +11,8 @@ function getRandom(min, max) {
 		$("header .menu-button").click();
 		$('html,body').animate({scrollTop:$("#"+$(this).attr("data-to")).offset().top-70}, 900);
 	});
-	$("footer .refers ul li a, .menu-item").mouseover(function(){
-		$(this).css("background", "rgba("+Math.round(getRandom(100, 255))+","+Math.round(getRandom(100, 255))+","+Math.round(getRandom(100, 255))+", .7)");
-	});
-	$("footer .refers ul li a, .menu-item").mouseout(function(){
-		$(this).css("background", "transparent");
-	});
+	$("footer .refers ul li a, .menu-item").css("background", "rgba("+Math.round(getRandom(100, 255))+","+Math.round(getRandom(100, 255))+","+Math.round(getRandom(100, 255))+", .7)");
+
 
 
 });
