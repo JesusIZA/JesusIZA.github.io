885789a21fd02a4f652678c00875689f1f889499
Jesus <true.is.us@gmail.com>
2020-07-03 13:41:49 +0300

    BodyaSite

--- old/test/scripts/appear.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	$(document.body).on('appear', '.feed', function(e, $affected) {
-		$(this).addClass("appeared");
-	});
-	$('.feed').appear({force_process: true});
-});
--- scripts/redirect.js
+++
@@ -1,6 +0,0 @@
-if(window.innerWidth > window.innerHeight && window.innerWidth > 1190) {
-	window.location.href = "pc.html";
-} else {
-	console.log(window.innerWidth);
-	window.location.href = "mobile.html";
-}
